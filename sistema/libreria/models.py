79d6e71848e4127732a70d583aee082589208cc6
FabrizioArnoletto <fearnolettodifilippo@escuelasproa.edu.ar>
2023-10-17 14:02:37 -0300

    Soluciones 1

--- sistema/libreria/models.py
+++ sistema/libreria/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Libro(models.Model):
     id=models.AutoField(primary_key=True)
     titulo=models.CharField(max_length=100,verbose_name="Titulo")
-    imagen=models.ImageField(upload_to='/imagenes',verbose_name="Imagen",null=True)
+    imagen=models.ImageField(upload_to='imagenes/',verbose_name="Imagen",null=True)
     descripcion=models.TextField(null=True,verbose_name="Descripcion")
