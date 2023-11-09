f4cd951c507241ea1f515bc1206d1b4a8f956d21
FabrizioArnoletto <fearnolettodifilippo@escuelasproa.edu.ar>
2023-11-09 11:36:38 -0300

    comentarios

--- sistema/libreria/models.py
+++ sistema/libreria/models.py
@@ -14,3 +14,12 @@ class Libro(models.Model):
     def delete(self, using=None, Keep_parents=False):
         self.Imagen.storage.delete(self.Imagen.name)
         super().delete()
+
+class comentarios(models.Model):
+    ID_c=models.AutoField(primary_key=True)
+    ID=models.ForeignKey(Libro, on_delete=models.CASCADE)
+    Comentario=models.TextField(null=True,verbose_name="Comentario")
+    
+    def __str__(self):
+        fila= "ID_c-ID: " + self.ID_c + "-" + self.ID +"Comentario" + self.Comentario
+        return fila
